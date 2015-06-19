cc62455a6e3e3318cd18ca8fe107b0deb287d078
Eli Smukler <eli@berecruited.com>
2015-06-19 00:30:27 -0700

    move more files

--- assets/boxOffice.js
+++ assets/boxOffice.js
@@ -1,29 +1,42 @@
-(function() {    
+(function() {
+
+    var makeRowFor = function(movie) {
+        $('table').removeClass("hidden");
+
+        $('tbody').append(
+            "<tr>" +
+            "<td>" + movie.title + "</td>" +
+            "<td>" + movie.year + "</td>" +
+            "<td>" + movie.budget + "</td>" +
+            "<td>" + movie.revenue + "</td>" +
+            "</tr>"
+        );
+    };
+
     var getBoxOfficeFor = function(id) {
         $.ajax({
             url: "http://api.themoviedb.org/3/movie/" + id + "?api_key=a1d5f291d84e71e51b248b86ec9c9e2a",
             type: "GET",
             success: function(data) {
                 var movie = {
-                  title: data.title,
-                  year: data.release_date.slice(0,4),
-                  budget: data.budget,
-                  revenue: data.revenue
+                    title: data.title,
+                    year: data.release_date.slice(0,4),
+                    budget: data.budget,
+                    revenue: data.revenue
                 }
                 makeRowFor(movie);
             }
         })
-    }
+    };
 
     var getMoviesOf = function(id) {
         $.ajax({
             url: "http://api.themoviedb.org/3/person/" + id + "/movie_credits?api_key=a1d5f291d84e71e51b248b86ec9c9e2a",
             type: "GET",
             success: function(data) {
-                getBoxOfficeFor(data.cast[0].id);
-                // data.cast.forEach(function(movie) {
-                //     getBoxOfficeFor(movie.id);
-                // })
+                data.cast.forEach(function(movie) {
+                    getBoxOfficeFor(movie.id);
+                })
             }
         });
     };
@@ -41,6 +54,7 @@
 
     $('form.search').submit(function(event) {
         event.preventDefault();
+        $('tbody').empty();
         var query = $('input').val();
         getIdOf(query);
     })
